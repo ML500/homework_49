e241c2b001778f8029c807215e8cb668fe2fba43
Emil <emilrakaev@gmail.com>
2020-08-06 00:37:00 +0600

    Добавлены представления и шаблоны главной и одной страниц

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import IndexView, GoalView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('goal/<int:pk>/', GoalView.as_view(), name='goal_view'),
+
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotAllowed
+from django.views.generic import View, TemplateView
+from webapp.models import Goal, Status, Type
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        goal = Goal.objects.all()
+
+        context['goals'] = goal
+        return context
+
+
+class GoalView(TemplateView):
+    template_name = 'goal_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        pk = self.kwargs.get('pk')
+        goal = get_object_or_404(Goal, pk=pk)
+
+        context['goal'] = goal
+        return context
+
+class GoalCreateView(View):
+    def get(self, request):
+        return render(request, 'goal_create.html', context={
+            'form': GoalForm()
+        })
+
+    def post(self, request):
+        form = GoalForm(data=request.POST)
+        if form.is_valid():
+            data = {}
+            for key, value in form.cleaned_data.items():
+                if value is not None:
+                    data[key] = value
+            goal = Goal.objects.create(**data)
+            return redirect('goal_view', pk=goal.pk)
+        else:
+            return render(request, 'goal_create.html', context={
+                'form': form
+            })
+
