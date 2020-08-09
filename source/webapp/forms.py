69432e4829f6ed2aea685df14559de9fe9ea8cc5
Emil <emilrakaev@gmail.com>
2020-08-09 17:36:11 +0600

    Переделаны классы создания и редактирования на FormView

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,5 +5,5 @@ from .models import Goal, Status, Type
 class GoalForm(forms.Form):
     summary = forms.CharField(max_length=200, required=True, label='Заголовок')
     description = forms.CharField(max_length=300, required=False, label='Описание', widget=forms.Textarea)
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус')
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), initial=Status.objects.first(),required=True, label='Статус')
     type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=False, label='Тип')
