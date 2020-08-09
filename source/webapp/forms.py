3733bad41583526a30029f420473bc87ff3c26f3
Emil <emilrakaev@gmail.com>
2020-08-09 23:39:42 +0600

    добавлен виджет для поля тип в forms.py

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,5 +5,6 @@ from .models import Goal, Status, Type
 class GoalForm(forms.Form):
     summary = forms.CharField(max_length=200, required=True, label='Заголовок')
     description = forms.CharField(max_length=300, required=False, label='Описание', widget=forms.Textarea)
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), initial=Status.objects.first(),required=True, label='Статус')
-    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=False, label='Тип')
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), initial=0, required=True, label='Статус')
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), widget=forms.CheckboxSelectMultiple,
+                                          required=False, label='Тип')
