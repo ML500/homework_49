3247bf0a899724649bf64acc84c26eee8cea0e4d
Emil <emilrakaev@gmail.com>
2020-08-09 17:34:21 +0600

    Переделаны классы создания и редактирования на FormView

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -6,4 +6,4 @@ class GoalForm(forms.Form):
     summary = forms.CharField(max_length=200, required=True, label='Заголовок')
     description = forms.CharField(max_length=300, required=False, label='Описание', widget=forms.Textarea)
     status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус')
-    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=True, label='Тип')
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=False, label='Тип')
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.http import HttpResponseNotAllowed
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, FormView
 from django.utils.timezone import make_naive
 
 from webapp.models import Goal, Status, Type
 from webapp.forms import GoalForm
+from .base_views import FormView as CustomFormView
 
 
 class IndexView(TemplateView):
@@ -32,62 +34,77 @@ class GoalView(TemplateView):
         return context
 
 
-class GoalCreateView(View):
-    def get(self, request):
-        return render(request, 'goal_create.html', context={
-            'form': GoalForm()
-        })
+# class GoalCreateView(CustomFormView):
+#     template_name = 'goal_create.html'
+#     form_class = GoalForm
+#
+#     def form_valid(self, form):
+#         data = {}
+#         type = form.cleaned_data.pop('type')
+#         for key, value in form.cleaned_data.items():
+#             if value is not None:
+#                 data[key] = value
+#         self.goal = Goal.objects.create(**data)
+#         self.goal.type.set(type)
+#         return super().form_valid(form)
+#
+#     def get_redirect_url(self):
+#         return reverse('goal_view', kwargs={'pk': self.goal.pk})
 
-    def post(self, request):
-        form = GoalForm(data=request.POST)
-        if form.is_valid():
-            data = {}
-            for key, value in form.cleaned_data.items():
-                if value is not None:
-                    data[key] = value
-            goal = Goal.objects.create(**data)
-            return redirect('goal_view', pk=goal.pk)
-        else:
-            return render(request, 'goal_create.html', context={
-                'form': form
-            })
+class GoalCreateView(FormView):
+    template_name = 'goal_create.html'
+    form_class = GoalForm
+
+    def form_valid(self, form):
+        data = {}
+        type = form.cleaned_data.pop('type')
+        for key, value in form.cleaned_data.items():
+            if value is not None:
+                data[key] = value
+        self.goal= Goal.objects.create(**data)
+        self.goal.type.set(type)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('goal_view', kwargs={'pk': self.goal.pk})
 
 
-class GoalUpdateView(TemplateView):
+class GoalUpdateView(FormView):
     template_name = 'goal_update.html'
+    form_class = GoalForm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.goal = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        pk = self.kwargs.get('pk')
-        goal = get_object_or_404(Goal, pk=pk)
-
-        initial = {}
-        for key in 'summary', 'description', 'status', 'type':
-            initial[key] = getattr(goal, key)
-        initial['created_at'] = make_naive(goal.created_at).strftime('%Y-%m-%dT%H:%M')
-        form = GoalForm(initial=initial)
-
-        context['goal'] = goal
-        context['form'] = form
-
+        context['goal'] = self.get_object()
         return context
 
-    def post(self, request, *args, **kwargs):
+    def get_initial(self):
+        initial = {}
+        for key in 'summary', 'description', 'status':
+            initial[key] = getattr(self.goal, key)
+        initial['created_at'] = make_naive(self.goal.created_at).strftime('%Y-%m-%dT%H:%M')
+        initial['type'] = self.goal.type.all()
+        return initial
+
+    def form_valid(self, form):
+        type = form.cleaned_data.pop('type')
+        for key, value in form.cleaned_data.items():
+            if value is not None:
+                setattr(self.goal, key, value)
+        self.goal.save()
+        self.goal.type.set(type)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('goal_view', kwargs={'pk': self.goal.pk})
+
+    def get_object(self):
         pk = self.kwargs.get('pk')
-        goal = get_object_or_404(Goal, pk=pk)
-        form = GoalForm(data=request.POST)
-        if form.is_valid():
-            for key, value in form.cleaned_data.items():
-                if value is not None:
-                    setattr(goal, key, value)
-            goal.save()
-            return redirect('goal_view', pk=goal.pk)
-        else:
-            return self.render_to_response({
-                'goal': goal,
-                'form': form
-            })
+        return get_object_or_404(Goal, pk=pk)
 
 
 class GoalDeleteView(TemplateView):
