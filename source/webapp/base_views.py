de321211a63a0fc560d4737aecb8d526378001f2
Emil <emilrakaev@gmail.com>
2020-08-20 00:54:08 +0600

    создал класс SearchView, наследовал его у indexVIew добавил стилизацию

--- source/webapp/base_views.py
+++ source/webapp/base_views.py
@@ -1,5 +1,7 @@
-from django.views.generic import View
+from django.db.models import Q
+from django.views.generic import View, ListView
 from django.shortcuts import render, redirect
+from webapp.forms import SimpleSearchForm
 
 
 class FormView(View):
@@ -31,3 +33,40 @@ class FormView(View):
 
     def get_redirect_url(self):
         return self.redirect_url
+
+
+class SearchView(ListView):
+    template_name = None
+    context_object_name = None
+    model = None
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        form = SimpleSearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            kwargs['search'] = search
+        kwargs['form'] = form
+        return super().get_context_data(object_list=object_list, **kwargs)
+
+    def get_queryset(self):
+        data = self.model.objects.all()
+        form = SimpleSearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            if search:
+                data = data.filter(self.get_query(search))
+        return data.order_by('-created_at')
+
+    def get_query(self, search):
+        query = None
+        return query
+
+    # def get_queryset(self):
+    #     data = self.model.objects.all()
+    #     form = self.search_form(data=self.request.GET)
+    #     if form.is_valid():
+    #         search = form.cleaned_data['search']
+    #         if search:
+    #             data = data.filter(Q(summary__icontains=search) | Q(description__icontains=search))
+    #
+    #     return data.order_by('-created_at')
